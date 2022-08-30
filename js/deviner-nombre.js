a482cdfa782c6b6016da5a77210297bb684da2f0
Colin Reist <colinreist04@gmail.com>
2022-08-30 10:38:39 +0200

    premier code

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,13 +1,16 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
+* @author  Colin Reist
 * @version 0.1
 * @since   2018-09-09 (date de création)
 */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
+
+    const valMin = 1;
+    const valMax = 100;
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
@@ -19,4 +22,22 @@
         return Math.floor(Math.random() * (max - min) + min);
     }
 
+    let nbMystere = tireNombre(valMin, valMax);
+    console.log(nbMystere);
+
+    let nbessaie = 0;
+    let controle = false;
+    do {
+        let valUtilisateur = prompt('le nombre est entre 1 et 100');
+
+        if (valUtilisateur < nbMystere) {
+            window.alert('plus grand');
+        } else if (valUtilisateur > nbMystere) {
+            window.alert('plus petit');
+        } else {
+            controle = true;
+        }
+        nbessaie++;
+    } while(!controle);
+    window.alert(`Félicitation tu as trouvé en ${nbessaie} et c'était le nombre ${nbMystere}`);
 }()); // main IIFE
